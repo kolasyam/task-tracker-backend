261b9b85381bad68fbf955d3cfb1eff52d2efafb
kolasyam <syammanikantakola426@gmail.com>
2025-04-27 12:47:58 +0530

    third commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
-require("./config/db");
+require("./config/db")();
 const PORT = process.env.PORT || 8000;
 app.get("/ping", (req, res) => {
   res.send("PONGI");
@@ -13,6 +13,12 @@ app.use(cors());
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/project", require("./routes/projectRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
+// After all your routes
+app.use((err, req, res, next) => {
+  console.error(err.stack); // this will print the error in Render logs
+  res.status(500).json({ message: err.message }); // show actual error to frontend/postman
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
